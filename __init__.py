47e8669c53126229dd26e73af785b5b66f63b2bb
Matthew <akinolatolulope24@gmail.com>
2022-08-11 23:34:07 +0100

    vdfe

--- __init__.py
+++ __init__.py
@@ -3,7 +3,7 @@ from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = 'wftdgvyfy2447ygghvhhy8ujiigi7yj988ui87'
-app.config["MONGO_URI"] = "mongodb+srv://Respect:<respect001>@cluster0.fclyhzv.mongodb.net/?retryWrites=true&w=majority"
+app.config["MONGO_URI"] = "mongodb+srv://Respect:respect001@cluster0.fclyhzv.mongodb.net/test?retryWrites=true&w=majority"
 
 # "mongodb+srv://Respect:<respect001>@cluster0.fclyhzv.mongodb.net/?retryWrites=true&w=majority"
 
@@ -13,4 +13,4 @@ from routers import *
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
